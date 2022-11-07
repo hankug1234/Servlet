8493af74e005212ed305bda50a670bbc6658a110
hankug1234 <hankug12@gmail.com>
2022-11-07 21:24:55 +0900

    jtamplate

--- pro19/src/com/spring/ex01/PersonTest.java
+++ pro19/src/com/spring/ex01/PersonTest.java
@@ -2,11 +2,20 @@ package com.spring.ex01;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 public class PersonTest {
 	public static void main(String[] args) {
-		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("person.xml"));
+		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("person.xml"));
+		ApplicationContext context = new ClassPathXmlApplicationContext("person.xml");
+		PersonService person0 = (PersonService) context.getBean("personService");
+		person0.sayHello();
+		/*
+		PersonService person0 = (PersonService) context.getBean("personService");
+		person0.sayHello();
+		
 		PersonService person = (PersonService) factory.getBean("personService");
 		person.sayHello();
 		
@@ -15,6 +24,7 @@ public class PersonTest {
 		
 		PersonService person2 = (PersonService) factory.getBean("personService2");
 		person2.sayHello();
+		*/
 	}
 
 }
